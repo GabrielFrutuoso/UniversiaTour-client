de3481fd31ab9052a4b9377e716148cc725d08ad
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-08 19:22:28 -0300

    added skelletons

--- src/components/MainPage/MainContent.tsx
+++ src/components/MainPage/MainContent.tsx
@@ -1,21 +1,20 @@
 import DestinyCard from "./DestinyCard"
+import { SkelletonDestiny } from "./Skelleton.Destiny"
 
 export const MainContent = () => {
   return (
     <main className="min-h-screen">
       <h1 className="text-center text-6xl my-16">Destinos</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 w-9/12 mx-auto">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 max-w-screen-xl mx-auto">
         <DestinyCard />
         <DestinyCard />
         <DestinyCard />
         <DestinyCard />
-        <DestinyCard />
-        <DestinyCard />
-        <DestinyCard />
-        <DestinyCard />
-        <DestinyCard />
-        <DestinyCard />
+        <SkelletonDestiny />
+        <SkelletonDestiny />
+        <SkelletonDestiny />
+        <SkelletonDestiny />
       </div>
     </main>
   )
---
+++ src/components/MainPage/Skelleton.Destiny.tsx
@@ -0,0 +1,6 @@
+
+export const SkelletonDestiny = () => {
+  return (
+    <div className="md:h-72 md:w-72 h-36 w-36 animate-pulse bg-neutral-300 rounded-md"></div>
+  )
+}
