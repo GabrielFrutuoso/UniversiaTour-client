8610835fca121ef82cc8cb10a53feba0b2fee277
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-08 18:23:14 -0300

    fixing navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,12 @@
+import { BsBackpack2Fill } from "react-icons/bs"
+
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header className="bg-white dark:bg-gray-800 w-full p-5 z-40 fixed top-0 left-0">
+      <div className="flex items-center gap-2">
+        <span className="text-3xl text-gray-600 dark:text-gray-200"><BsBackpack2Fill /></span>
+        <h1 className="text-3xl text-gray-600 dark:text-gray-200">UniversiaTour</h1>
+      </div>
+    </header>
   )
 }
--- src/pages/Main.page.tsx
+++ src/pages/Main.page.tsx
@@ -1,7 +1,18 @@
+import { Footer } from "../components/Footer"
 import { Hero } from "../components/MainPage/Hero"
+import { NavBar } from "../components/NavBar"
 
 export const MainPage = () => {
   return (
+    <>
+    <NavBar />
     <Hero />
+    <main className="h-[100vh]">
+
+    </main>
+    <Footer />
+    </>
+    
+
   )
 }
