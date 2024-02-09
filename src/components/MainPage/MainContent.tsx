bc889fd1194fc04bd4e8b5180b25503ca4949e3b
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-09 17:20:25 -0300

    fix: hiding .env file

--- src/components/MainPage/MainContent.tsx
+++ src/components/MainPage/MainContent.tsx
@@ -19,7 +19,7 @@ export const MainContent = () => {
 
   return (
     <main className="min-h-screen">
-      <h1 className="text-center text-6xl my-16">Destinos</h1>
+      <h1 className="text-center text-6xl my-8">Destinos</h1>
 
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 max-w-screen-xl mx-auto">
           {isLoading ? <SkelletonsPlaceholder /> : data?.map((destiny: Destiny) => (
